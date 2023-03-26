aacac99313fb3d38dc4579a07ad5590a9e4cc53a
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-03-26 19:42:16 +0530

    added pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import CN from "./components/CN"
+import OS from "./components/OS"
+import DHT from "./components/DHT"
+import Rust from './components/Rust'
+import cliDoc from './components/CLI-doc'
 import './index.css'
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/cli",
+    element: <cliDoc/>
+  },
+  {
+    path: "/computer-networking",
+    element: <CN />
+  },
+  {
+    path: "/rust",
+    element: <Rust />
+  },
+  {
+    path: "/os",
+    element: <OS />
+  },
+  {
+    path: "/dht",
+    element: <DHT />
+  }
+
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
