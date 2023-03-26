341e06cab098066a42dae1bd98d28f248b6319cc
Vedant_Tarale <vedanttarale.211ec257@nitk.edu.in>
2023-03-26 14:42:07 +0530

    designed basic layout for CN blog

---
+++ frontend/src/components/CN_topics/Model.jsx
@@ -0,0 +1,8 @@
+function Model() {
+    return (
+        <div className="text-black">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aspernatur cumque ipsum alias minus, tenetur repellat non qui officia provident fugit ex maxime, officiis commodi sint rerum voluptatibus. Impedit in, atque tempore voluptatem ipsum fugiat corporis possimus nobis temporibus cumque molestiae maxime! Inventore culpa aliquam porro! Quos quis excepturi fuga labore blanditiis exercitationem libero aliquam explicabo! Eveniet, in voluptate animi quae, harum ab officiis architecto fugit sapiente deleniti ea reprehenderit?
+        </div>
+    )
+}
+export default Model
