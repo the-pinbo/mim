d128ffba5806504f9ffedb40f368617d3b86b706
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-08-05 14:35:49 +0530

    my github url

--- frontend/src/components/AboutCLI.jsx
+++ frontend/src/components/AboutCLI.jsx
@@ -10,7 +10,7 @@ function AboutCLI() {
                 <p className="text-[#00df9a] font-bold">CLI Docs</p>
                 <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">HOW TO USE THE CLI?</h1>
                 <p>Get a comprehensive idea of how to use our CLI tool and explore all its functionalities!</p>
-                <a href="https://github.com/the-pinbo/mim#how-to-install" target='blank' ><button className='bg-black w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-4 text-center text-[#00df9a]'>Get Started</button></a>
+                <a href="https://github.com/ahmedfahim21/mim/tree/main#how-to-install" target='blank' ><button className='bg-black w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-4 text-center text-[#00df9a]'>Get Started</button></a>
                 </div>
         </div>
       
